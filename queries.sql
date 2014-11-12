49189e187924997e46ccc10ace2d090342059ff9
luzhin <luzhin.kirill@yandex.ru>
2014-11-12 12:51:01 +0200

    TO. 0.3. Create table скрипты.

--- queries.sql
+++ queries.sql
@@ -12,3 +12,52 @@ CREATE TABLE `w_to` (
      `seo_descr` varchar(512) DEFAULT NULL
 ) DEFAULT CHARSET=utf8;
 
+CREATE TABLE `w_to_types` (
+     `id` INT(10) UNSIGNED NOT NULL,
+     `model_id` INT(10) UNSIGNED NOT NULL,
+     `name` varchar(100) DEFAULT NULL,
+     `sort` INT(10) UNSIGNED DEFAULT NULL,
+     `is_active` INT(10) UNSIGNED DEFAULT NULL,
+     `content` varchar(100) DEFAULT NULL,
+     `title` varchar(100) DEFAULT NULL,
+     `kwords` varchar(100) DEFAULT NULL,
+     `descr` varchar(100) DEFAULT NULL,
+     `img` varchar(100) DEFAULT NULL,
+     `mod` varchar(100) DEFAULT NULL,
+     `engine` varchar(100) DEFAULT NULL,
+     `engine_model` varchar(100) DEFAULT NULL,
+     `engine_obj` varchar(100) DEFAULT NULL,
+     `engine_horse` varchar(100) DEFAULT NULL,
+     `type_year` varchar(100) DEFAULT NULL,
+     `seo_text` varchar(100) DEFAULT NULL,
+     `tecdoc_url` varchar(100) DEFAULT NULL,
+     `tecdoc_id` INT(10) UNSIGNED DEFAULT NULL
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `w_to_models` (
+     `id` INT(10) UNSIGNED NOT NULL,
+     `car_id` INT(10) UNSIGNED DEFAULT NULL,
+     `name` varchar(100) DEFAULT NULL,
+     `sort` INT(10) UNSIGNED DEFAULT NULL,
+     `is_active` INT(10) UNSIGNED DEFAULT NULL,
+     `content` varchar(100) DEFAULT NULL,
+     `title` varchar(100) DEFAULT NULL,
+     `kwords` varchar(100) DEFAULT NULL,
+     `descr` varchar(100) DEFAULT NULL,
+     `img` varchar(300) DEFAULT NULL,
+     `seo_text` varchar(100) DEFAULT NULL
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `w_to_cars` (
+     `id` INT(10) UNSIGNED NOT NULL,
+     `name` varchar(100) DEFAULT NULL,
+     `sort` INT(10) UNSIGNED DEFAULT NULL,
+     `is_active` INT(10) UNSIGNED DEFAULT NULL,
+     `content` varchar(100) DEFAULT NULL,
+     `title` varchar(100) DEFAULT NULL,
+     `kwords` varchar(100) DEFAULT NULL,
+     `descr` varchar(100) DEFAULT NULL,
+     `img` varchar(300) DEFAULT NULL,
+     `truck` INT(10) UNSIGNED DEFAULT NULL,
+     `seo_text` varchar(100) DEFAULT NULL
+) DEFAULT CHARSET=utf8;
